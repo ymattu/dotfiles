37572e60dc98043372d05704f5c874bb114024e0
Yuya Matsumura <mattu.yuya@gmail.com>
2018-08-30 01:37:26 +0900

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# dotfiles path
-DOTFILES_PATH="$HOME/dotfiles"
+# util functions
+print_error() {
+    printf "\033[31m    [×] $1\033[m\n"
+}
 
-# Load utils
-. $DOTFILES_PATH/setup/utils.sh
+print_success() {
+    printf "\033[32m    [✓] $1\033[m\n"
+}
+
+print_warning() {
+    printf "\033[33m    [!] $1\033[m\n"
+}
+
+print_title() {
+    printf "\n\n\033[35m$1\033[m\n\n"
+}
+
+print_message() {
+    printf "    $1\n"
+}
+######################################
 
 check_os() {
     os_name="$(uname)"
