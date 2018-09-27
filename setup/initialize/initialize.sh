3c80b0d7c4834dd662f2fb9141623bc694517158
Yuya Matsumura <mattu.yuya@gmail.com>
2018-09-27 15:16:42 +0900

    change directory name

--- setup/initialize/initialize.sh
+++ setup/initialize/initialize.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-. setup/initialize/xcode.sh
-. setup/initialize/brew.sh
-. setup/initialize/gdrive.sh
-. setup/initialize/fonts.sh
-. setup/initialize/emacs.sh
-. setup/initialize/rust.sh
-. setup/initialize/node.sh
-. setup/initialize/gem.sh
-. setup/initialize/python.sh
-. setup/initialize/java.sh
-. setup/initialize/R.sh
-. setup/initialize/iterm2.sh
+# dotfiles path
+DIR_PATH="$HOME/dotfiles"
+
+
+. $DIR_PATH/setup/initialize/xcode.sh
+. $DIR_PATH/setup/initialize/brew.sh
+. $DIR_PATH/setup/initialize/gdrive.sh
+. $DIR_PATH/setup/initialize/fonts.sh
+. $DIR_PATH/setup/initialize/emacs.sh
+. $DIR_PATH/setup/initialize/rust.sh
+. $DIR_PATH/setup/initialize/node.sh
+. $DIR_PATH/setup/initialize/gem.sh
+. $DIR_PATH/setup/initialize/python.sh
+. $DIR_PATH/setup/initialize/java.sh
+. $DIR_PATH/setup/initialize/R.sh
+. $DIR_PATH/setup/initialize/iterm2.sh
